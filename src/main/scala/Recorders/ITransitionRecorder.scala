e9d8511824df4f73649cbf98c8773e8ea866d367
tenormadness <luca.tosatto@gmail.com>
2016-06-29 18:22:15 -0400

    added some test support

--- src/main/scala/Recorders/ITransitionRecorder.scala
+++ src/main/scala/Recorders/ITransitionRecorder.scala
@@ -8,6 +8,7 @@ trait ITransitionRecorder {
 
   def recordTransition(in: Transition): Unit
 
+  /** a little helper function that build the transition for you starting from two nodes */
   def recordTransition[T, TT](from: Traceable[T], to: Traceable[TT], tag: String = ""): Unit = {
 
     recordTransition(Transition(from.id, to.id, tag))
